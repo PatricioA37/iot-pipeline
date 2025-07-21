43f32453a5fee54a380f0ae4e9d9941bc0c0d2eb
PatricioA37 <patricio37alejandro@gmail.com>
2025-07-20 22:56:10 -0400

    primer nucleo: 001 prueba4

--- flask_agent/app.py
+++ flask_agent/app.py
@@ -1,33 +1,25 @@
-from flask import Flask
-from kafka import KafkaConsumer
-import paho.mqtt.publish as publish
-import threading
-import json
+from flask import Flask, request
+import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
 
-def consume_kafka():
-    consumer = KafkaConsumer(
-        'iot-temperatura',
-        bootstrap_servers='kafka:9092',
-        auto_offset_reset='earliest',
-        group_id='flask-agent',
-        enable_auto_commit=True
-    )
-    for message in consumer:
-        try:
-            temperatura = float(message.value.decode())
-            print(f"Temperatura recibida: {temperatura}")
-            if temperatura > 30:
-                print("¡Temperatura alta! Activando ventilador")
-                publish.single("actuador/ventilador", "ON", hostname="mosquitto")
-        except Exception as e:
-            print("Error:", e)
+mqtt_broker = "mosquitto"
+mqtt_port = 1883
+mqtt_topic = "iot/test"
 
-@app.route("/")
-def home():
-    return "Flask Agent corriendo"
+client = mqtt.Client()
+client.connect(mqtt_broker, mqtt_port, 60)
 
-if __name__ == "__main__":
-    threading.Thread(target=consume_kafka, daemon=True).start()
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/')
+def index():
+    return "MQTT Flask Agent Activo"
+
+@app.route('/publish', methods=['POST'])
+def publish():
+    data = request.json
+    msg = data.get("message", "Hola IoT")
+    client.publish(mqtt_topic, msg)
+    return {"status": "mensaje enviado", "mensaje": msg}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
